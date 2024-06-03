296366cffde474665e2f261d69216e180af3ac2f
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-06-03 12:39:09 -0700

    Made test class for uncertainty propagation methods

---
+++ src/test/tools/uncertainty_prop.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import numpy as np
+import GPy
+
+
+class TestUncertaintyProp(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_mean_prop(self):
+        self.assertTrue(True)
+
