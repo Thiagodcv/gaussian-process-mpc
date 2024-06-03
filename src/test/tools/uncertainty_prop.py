d4ab64ce53f2601e3eb12e72fab0d2cab4e77bfe
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-06-03 12:58:55 -0700

    Building test function for mean_prop method

--- src/test/tools/uncertainty_prop.py
+++ src/test/tools/uncertainty_prop.py
@@ -12,5 +12,25 @@ class TestUncertaintyProp(TestCase):
         pass
 
     def test_mean_prop(self):
-        self.assertTrue(True)
+        num_train = 100
 
+        # Generate noise
+        sigma = 0.5
+        err = np.random.normal(loc=1, scale=sigma, size=num_train)
+
+        # True function is f(x) = x_1^2 + x_2^2
+        def f(x):
+            return x.T @ np.identity(2) @ x
+
+        # Generate 2D Gaussian inputs
+        mu = np.array([5., 4.])
+        S = np.array([[2., 0.5],
+                      [0.5, 2.]])
+        X_train = np.random.multivariate_normal(mu, S, num_train)
+
+        # Generate scalar outputs
+        y_train = np.array([f(X_train[j, :]) for j in range(num_train)]) + err
+
+        self.assertEqual(X_train.shape[0], num_train)
+        self.assertEqual(X_train.shape[1], 2)
+        self.assertEqual(y_train.shape[0], num_train)
