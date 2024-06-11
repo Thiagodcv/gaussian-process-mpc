8800be48d61f12cd69a23281f6b045078f677b93
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-06-11 16:26:16 -0700

    Did a bit more testing of mixing casadi with numpy

--- src/test/test_casadi.py
+++ src/test/test_casadi.py
@@ -11,7 +11,7 @@ class TestCasadi(TestCase):
     def tearDown(self):
         pass
 
-    def test_Casadi(self):
+    def test_casadi(self):
         x = SX.sym('x')
         y = SX.sym('y')
         z = SX.sym('z')
@@ -23,3 +23,37 @@ class TestCasadi(TestCase):
         x_opt = r['x']
         print('x_opt: ', x_opt)
 
+    def test_casadi_function_call(self):
+
+        def f(a, b, c):
+            return a ** 2 + 100 * c ** 2
+
+        def g(a, b, c):
+            return c + (1-a)**2 - b
+
+        x = SX.sym('x')
+        y = SX.sym('y')
+        z = SX.sym('z')
+        nlp = {'x': vertcat(x, y, z), 'f': f(x, y, z), 'g': g(x, y, z)}
+        S = nlpsol('S', 'ipopt', nlp)
+        print(S)
+
+        r = S(x0=[2.5, 3.0, 0.75], lbg=0, ubg=0)
+        x_opt = r['x']
+        print('x_opt: ', x_opt)
+
+    def test_casadi_numpy_call(self):
+        H = np.identity(2)
+        x = MX.sym('x', 2)
+
+        def f(a):
+            return a.T @ H @ a
+
+        def g(a):
+            return x[0] - 1
+
+        nlp = {'x': x, 'f': f(x), 'g': g(x)}
+        S = nlpsol('S', 'ipopt', nlp)
+        r = S(x0=[-1, -1], lbg=0, ubg=0)
+        x_opt = r['x']
+        print('x_opt: ', x_opt)
