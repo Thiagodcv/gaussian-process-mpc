57bc48e60459e59bc5adf6054c5293ef29242e8a
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-07-24 21:01:36 -0700

    Implementing test to test forward_propagate_torch

--- src/experiments/test_experiment.py
+++ src/experiments/test_experiment.py
@@ -41,7 +41,28 @@ def test_experiment():
     mpc.set_xref(np.array([0.]))
     mpc.set_uref(np.array([0.]))
 
-    print("Stop here")
+    curr_state = np.array([5.])
+
+    # Compute cost at minimizing trajectory when curr_state = [5]
+    # mpc.last_traj = np.array([-1, -1, -1, -1, -1])  # set as initial starting point. Note this is not global min.
+    mpc.get_optimal_trajectory(curr_state)
+    # opt_u = mpc.last_traj  # Expect to get u = [-1, -1, -1, -1, -1]
+    # min_cost = mpc.objective(opt_u)
+    # min_grad = mpc.gradient(opt_u)
+    # print("Cost of IPOPT trajectory: ", min_cost)
+    # print("Gradient at IPOPT trajectory: ", min_grad)
+
+    intuit_traj = np.array([-1, -1, -1, -1, -1])
+    intuit_cost = mpc.objective(intuit_traj)
+    intuit_grad = mpc.gradient(intuit_traj)
+    print("Cost of intuitive trajectory: ", intuit_cost)
+    print("Gradient at intuitive trajectory: ", intuit_grad)
+
+    traj_d = np.array([-1, -1, -1, -1, -1.1])
+    traj_d_cost = mpc.objective(traj_d)
+    traj_d_grad = mpc.gradient(traj_d)
+    print("Cost of traj_d trajectory: ", traj_d_cost)
+    print("Gradient at traj_d trajectory: ", traj_d_grad)
 
 
 if __name__ == '__main__':
