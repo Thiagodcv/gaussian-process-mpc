a2ae700ba5300f5ddd0f682d813a9dc0f1d36390
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-07-25 11:36:22 -0700

    Found out that sigma_f = 1 breaks things

--- src/dynamics.py
+++ src/dynamics.py
@@ -125,6 +125,7 @@ class Dynamics(object):
         TODO: nominal models aren't taken into account here. Try to see if this can be remedied.
         TODO: Figure out why CUDA runs out of memory for large num_train.
         TODO: Figure out why covariance_prop_torch is much slower than for variance_prop_torch.
+        TODO: (mean/cov/var)_prop_torch all assume sigma_f = 1. Modify for general sigma_f.
         Given `horizon` number of actions, compute the expected states and state covariances.
         Note that because this method only takes actions as arguments (and not states), this method
         corresponds to a shooting method. This is implemented using Torch instead of NumPy.
