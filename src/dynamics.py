Fixing issues with backprop

src/dynamics.py
@@ -155,7 +155,7 @@ class Dynamics(object):
         y_train = self.gpr_err[0].y_train
 
         for t in range(1, horizon + 1):
-            print("Timestep: ", t)
+            # print("Timestep: ", t)
             mean = torch.concatenate((state_means[t - 1], actions[t - 1, :]))
             new_mean = torch.zeros((self.state_dim,), device=device).type(torch.float64)
             new_covar = torch.zeros((self.state_dim, self.state_dim), device=device).type(torch.float64)
@@ -204,6 +204,7 @@ class Dynamics(object):
                                                                   betas[i], betas[j])
 
             # Copy values from lower diagonal to upper diagonal
-            state_covars[t] += state_covars[t].mT - torch.diag(torch.diag(state_covars[t]))
+            # state_covars[t] += state_covars[t].mT - torch.diag(torch.diag(state_covars[t]))
+            state_covars[t] = state_covars[t] + state_covars[t].mT - torch.diag(torch.diag(state_covars[t]))
 
         return state_means, state_covars

src/experiments/test_experiment.py
@@ -4,6 +4,7 @@ from src.mpc import RiskSensitiveMPC
 from src.simulator import Simulator
 import os
 import cProfile
+import torch
 
 
 def test_experiment():
@@ -42,27 +43,30 @@ def test_experiment():
     mpc.set_uref(np.array([0.]))
 
     curr_state = np.array([5.])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    mpc.curr_state = torch.tensor(curr_state, device=device).type(torch.float64)
+    torch.autograd.set_detect_anomaly(True)
 
     # Compute cost at minimizing trajectory when curr_state = [5]
-    # mpc.last_traj = np.array([-1, -1, -1, -1, -1])  # set as initial starting point. Note this is not global min.
+    mpc.last_traj = np.array([-1, -1, -1, -1, -1])  # set as initial starting point. Note this is not global min.
     mpc.get_optimal_trajectory(curr_state)
-    # opt_u = mpc.last_traj  # Expect to get u = [-1, -1, -1, -1, -1]
-    # min_cost = mpc.objective(opt_u)
-    # min_grad = mpc.gradient(opt_u)
-    # print("Cost of IPOPT trajectory: ", min_cost)
-    # print("Gradient at IPOPT trajectory: ", min_grad)
+    opt_u = mpc.last_traj  # Expect to get u = [-1, -1, -1, -1, -1]
+    min_cost = mpc.objective(opt_u)
+    min_grad = mpc.gradient(opt_u)
+    print("Cost of IPOPT trajectory: ", min_cost)
+    print("Gradient at IPOPT trajectory: ", min_grad)
 
-    intuit_traj = np.array([-1, -1, -1, -1, -1])
-    intuit_cost = mpc.objective(intuit_traj)
-    intuit_grad = mpc.gradient(intuit_traj)
-    print("Cost of intuitive trajectory: ", intuit_cost)
-    print("Gradient at intuitive trajectory: ", intuit_grad)
-
-    traj_d = np.array([-1, -1, -1, -1, -1.1])
-    traj_d_cost = mpc.objective(traj_d)
-    traj_d_grad = mpc.gradient(traj_d)
-    print("Cost of traj_d trajectory: ", traj_d_cost)
-    print("Gradient at traj_d trajectory: ", traj_d_grad)
+    # intuit_traj = np.array([-1, -1, -1])
+    # intuit_cost = mpc.objective(intuit_traj)
+    # intuit_grad = mpc.gradient(intuit_traj)
+    # print("Cost of intuitive trajectory: ", intuit_cost)
+    # print("Gradient at intuitive trajectory: ", intuit_grad)
+    #
+    # traj_d = np.array([-1, -1, -1.1])
+    # traj_d_cost = mpc.objective(traj_d)
+    # traj_d_grad = mpc.gradient(traj_d)
+    # print("Cost of traj_d trajectory: ", traj_d_cost)
+    # print("Gradient at traj_d trajectory: ", traj_d_grad)
 
 
 if __name__ == '__main__':

src/mpc.py
@@ -156,11 +156,11 @@ class RiskSensitiveMPC:
 
         Parameters
         ----------
-        x: (horizon+1, state_dim) torch tensor
-            A trajectory of state means; Assume x[0, :] is the current state
+        x: list of length horizon+1 with (state_dim,) torch tensors
+            A trajectory of state means; Assume x[0] is the current state
         u: (horizon, input_dim) torch tensor
             A trajectory of inputs
-        sig: (horizon+1, state_dim, state_dim) torch tensor
+        sig: list of length horizon+1 with (state_dim, state_dim) torch tensor
             A trajectory of state covariance matrices
         x_ref: (state_dim,) torch tensor
             The reference (or setpoint) state
@@ -177,12 +177,13 @@ class RiskSensitiveMPC:
 
         cost = 0
         for i in range(self.horizon + 1):
-            cost += (1/self.gamma * torch.log(torch.linalg.det(torch.eye(self.state_dim, device=self.device) +
-                                                                 self.gamma * self.Q_tor @ sig[i, :, :])))
-            cost += (x[i, :] - x_ref) @ torch.linalg.inv(Q_inv + self.gamma * sig[i, :, :]) @ (x[i, :] - x_ref)
+            cost = cost + (1/self.gamma * torch.log(torch.linalg.det(torch.eye(self.state_dim, device=self.device) +
+                                                                 self.gamma * self.Q_tor @ sig[i])))
+            cost = cost + (x[i] - x_ref) @ torch.linalg.inv(Q_inv + self.gamma * sig[i]) @ (x[i] - x_ref)
+            # cost += (x[i, :] - x_ref) @ self.Q_tor @ (x[i, :] - x_ref)  # for testing purposes only
 
         for j in range(self.horizon):
-            cost += (u[j, :] - u_ref) @ self.R_tor @ (u[j, :] - u_ref)
+            cost = cost + (u[j, :] - u_ref) @ self.R_tor @ (u[j, :] - u_ref)
 
         if self.R_delta_tor is not None:
             last_u = torch.tensor(self.last_traj[0:self.input_dim], device=self.device).type(torch.float64)
@@ -191,7 +192,7 @@ class RiskSensitiveMPC:
             delta_u = torch.diff(u_expanded, dim=0)
 
             for j in range(self.horizon):
-                cost += delta_u[j, :] @ self.R_delta_tor @ delta_u[j, :]
+                cost = cost + delta_u[j, :] @ self.R_delta_tor @ delta_u[j, :]
 
         return cost
 
@@ -208,13 +209,14 @@ class RiskSensitiveMPC:
         -------
         scalar
         """
-        # print("objective")
+        print("objective")
         u = torch.as_tensor(x.reshape(self.horizon, self.input_dim), device=self.device).type(torch.float64)
         u.requires_grad_(True)
         x_init = self.curr_state
 
         state_means, state_covars = self.dynamics.forward_propagate_torch(self.horizon, x_init, u)
         cost = self.cost_torch(state_means, u, state_covars, self.x_ref, self.u_ref)
+        # print("mean states: ", state_means)
 
         self.curr_cost = cost
         self.curr_u = u
@@ -235,14 +237,14 @@ class RiskSensitiveMPC:
         -------
         scalar
         """
-        # print("gradient")
+        print("gradient")
         if self.curr_cost is None:
             self.objective(x)
 
         if self.backward_taken:
             return self.curr_grad
         else:
-            self.curr_cost.backward()
+            self.curr_cost.backward(retain_graph=True)
             self.backward_taken = True
             self.curr_grad = self.curr_u.grad.cpu().detach().numpy()
             return self.curr_grad
@@ -294,7 +296,7 @@ class RiskSensitiveMPC:
         nlp.add_option('accept_every_trial_step', 'yes')  # Disable line search
 
         # Trying to loosen convergence constraints to converge earlier... doesn't really seem to help
-        nlp.add_option('max_iter', 10)  # default 3000
+        # nlp.add_option('max_iter', 10)  # default 3000
         # nlp.add_option('tol', 1e-1)  # default 1e-8
         # nlp.add_option('acceptable_tol', 1e-1)  # default 1e-6
         # nlp.add_option('constr_viol_tol', 1e-1)  # default 1e-8

src/test/test_mpc.py
@@ -131,6 +131,7 @@ class TestRiskSensitiveMPC(TestCase):
         mpc.dynamics.append_train_data(state, action, next_state)
 
         curr_state = np.array([0., 0.])
+        torch.autograd.set_detect_anomaly(True)
         opt_traj = mpc.get_optimal_trajectory(curr_state)
         self.assertTrue(opt_traj.shape == (horizon, input_dim))
 
@@ -181,12 +182,12 @@ class TestRiskSensitiveMPC(TestCase):
         x_traj[1, :] = np.array([2, 2])
         x_traj[2, :] = np.array([3, 3])
         u_traj[0, :] = np.array([2, 2])
-        u_traj[0, :] = np.array([4, 4])
+        u_traj[1, :] = np.array([4, 4])
         sig_traj[0, :, :] = np.array([[1, 2],
                                       [3, 4]])
         sig_traj[1, :, :] = np.array([[5, 6],
                                       [7, 8]])
-        sig_traj[1, :, :] = np.array([[9, 10],
+        sig_traj[2, :, :] = np.array([[9, 10],
                                       [11, 12]])
         gamma = 1.1
 
@@ -198,7 +199,7 @@ class TestRiskSensitiveMPC(TestCase):
 
         # Manually compute cost
         cost = 0
-        cost += 1/gamma * np.log(np.linalg.det(np.identity(state_dim) + gamma * Q @ sig_traj[0, :, :]))
+        cost += 1 / gamma * np.log(np.linalg.det(np.identity(state_dim) + gamma * Q @ sig_traj[0, :, :]))
         cost += 1 / gamma * np.log(np.linalg.det(np.identity(state_dim) + gamma * Q @ sig_traj[1, :, :]))
         cost += 1 / gamma * np.log(np.linalg.det(np.identity(state_dim) + gamma * Q @ sig_traj[2, :, :]))
 
@@ -220,11 +221,18 @@ class TestRiskSensitiveMPC(TestCase):
         # Compute cost using torch
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         x_traj = torch.tensor(x_traj, device=device).to(torch.float64)
+        x_traj = [x_traj[0, :], x_traj[1, :], x_traj[2, :]]
+
         u_traj = torch.tensor(u_traj, device=device).to(torch.float64)
+
         sig_traj = torch.tensor(sig_traj, device=device).to(torch.float64)
+        sig_traj = [sig_traj[0, :, :], sig_traj[1, :, :], sig_traj[2, :, :]]
+
         x_ref = torch.tensor(x_ref, device=device).to(torch.float64)
         u_ref = torch.tensor(u_ref, device=device).to(torch.float64)
 
+        # Now mpc.last_traj is generated randomly, so set to zeros for test to still work
+        mpc.last_traj = [0 for _ in range(N_c * input_dim)]
         cost_torch = mpc.cost_torch(x_traj, u_traj, sig_traj, x_ref, u_ref)
 
         print(cost)