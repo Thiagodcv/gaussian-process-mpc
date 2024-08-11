c02386a58b7711b2cb4441ae39c9e45ad2033850
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-08-10 18:14:23 -0700

    Made it so that simulator can record environments

--- src/simulator.py
+++ src/simulator.py
@@ -1,12 +1,13 @@
+import gym.wrappers
 import numpy as np
 
 
 class Simulator(object):
     """
-    Class with methods for simulating a GP-MPC setup in the given environment.
+    Class with methods for simulating a GP-MPC setup in the given environment. Comes with recording functionality.
     """
 
-    def __init__(self, mpc, env, num_iters=500):
+    def __init__(self, mpc, env, num_iters=500, record=False, video_folder=None, name_prefix=None):
         """
         Parameters:
         ----------
@@ -14,16 +15,33 @@ class Simulator(object):
         env: Gym environment
         num_iters: int
             Number of iterations to run the environment for
+        record: bool
+            Set to true to record the episode
+        video_folder: None or String
+            Path to folder where the video should be saved if record=True
+        name_prefix: None or String
+            Prefix to give to the video file name if record=True
         """
         self.mpc = mpc
-        self.env = env
         self.num_iters = num_iters
+
+        self.record = record
+        self.video_folder = video_folder
+        self.name_prefix = name_prefix
+        if record:
+            self.env = gym.wrappers.RecordVideo(env=env, video_folder=video_folder, name_prefix=name_prefix)
+        else:
+            self.env = env
 
     def run(self):
         """
         Simulates running a GP-MPC setup in the given environment.
         """
         obs, info = self.env.reset()
+        if self.record:
+            assert self.env.render_mode == 'rgb_array'
+            self.env.start_video_recorder()
+
         for t in range(self.num_iters):
             action = self.mpc.get_optimal_trajectory(obs)[0, :]
             next_obs, reward, terminated, truncated, _ = self.env.step(action)
@@ -36,4 +54,6 @@ class Simulator(object):
             self.mpc.dynamics.append_train_data(obs, action, next_obs)
             obs = next_obs
 
+        if self.record:
+            self.env.close_video_recorder()
         self.env.close()
